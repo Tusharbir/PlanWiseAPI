56b80ae8ae70d05c28f8207bb6bc20036fb6ccfa
kunalrastogi13 <kunalrastogi13@gmail.com>
2025-07-27 14:23:19 -0400

    add web crawler API

--- src/main/java/com/planwise/PlanWiseApplication.java
+++ src/main/java/com/planwise/PlanWiseApplication.java
@@ -2,8 +2,10 @@ package com.planwise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PlanWiseApplication {
 
     public static void main(String[] args) {
